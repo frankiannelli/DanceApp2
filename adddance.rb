4e4c691c8d29dc146374e5d02eca18ca0462191a
Dae <juliandaeone@gmail.com>
2018-03-01 17:57:59 +1100

    redecorated working app, file name changes and files deleted

--- adddance.rb
+++ adddance.rb
@@ -1,7 +1,7 @@
 module AddDance
     module_function
     def add_dance(library_data)
-        puts "What would you like to add?"
+        puts "What would you like to add?".colorize(:yellow)
         new_dance = gets.chomp
         if library_data.include? new_dance
             puts "#{new_dance} already exists in the Dance list!"
@@ -9,9 +9,7 @@ module AddDance
         elsif 
             library_data.push(new_dance)
             puts "this is the new complete list of moves #{library_data.columnize :displaywidth => 50, :colsep => ' | '}"
-        else
-            puts "how'd this happen?".colorize(:red)
-            AddDance.add_dance(library_data)
+      
         end
     end
 end
